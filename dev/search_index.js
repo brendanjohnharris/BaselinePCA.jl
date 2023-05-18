719034ff6f1ed21f8d3a0b0a941d69f87e68ba89
Documenter.jl <documenter@juliadocs.github.io>
2023-05-18 02:39:16 +0000

    build based on 0c51257

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BaselinePCA","category":"page"},{"location":"#BaselinePCA","page":"Home","title":"BaselinePCA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BaselinePCA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BaselinePCA]","category":"page"}]
+}
